b1955dc39bd5aa138b9693531541053122299d4c
rehmi <rehmi@users.noreply.github.com>
2024-04-30 22:35:40 +0000

    Deploying to gh-pages from  @ aced761111801d493155c8b433d5c53de843983a 🚀

--- assets/demos/code/_ceval_3.jl
+++
@@ -1,7 +0,0 @@
-# This file was generated, do not modify it. # hide
-s = "hello"
-struct T; v::Int; end
-[
-    Dict(:a => T(1)),
-    Dict(:b => T(2)),
-]
--- assets/demos/code/_ceval_4.jl
+++
@@ -1,3 +0,0 @@
-# This file was generated, do not modify it. # hide
-T(1)
-print(s)
